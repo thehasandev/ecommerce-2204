07925dc9ae90338bad3e1e96a5d13f9695fbdb3d
Hasan <thehasandev@gmail.com>
2023-11-01 23:20:37 +0600

    log-in-res

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -14,10 +14,10 @@ function Login() {
     let pageName = useSelector((state)=>state.bractcumb.previusName)
   
   return (
-    <Section className="mt-124">
+    <Section className=" mt-10 md:mt-124 px-4 xl:px-0">
         <Container>
         <SubHeading text="Login" className="mb-2"/>
-        <Flex className="items-center gap-x-2">
+        <Flex className="items-center gap-x-2 my-5">
               <p className='font-dm font-normal text-xs text-secondary first-letter:uppercase flex items-center gap-x-2'>
                  <Link to={pageName == "Home" ? "/" : pageName =="Sing up" ? "/sing-up" : `/${pageName}`}>
                     {pageName} 
@@ -27,9 +27,9 @@ function Login() {
               <p className='font-dm font-normal text-xs text-secondary first-letter:uppercase'>{window.location.pathname.replace("/"," ")}</p>
         </Flex>
             
-        <p className='font-dm font-normal text-gray w-664 mt-127 mb-57 text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+        <p className='font-dm font-normal text-gray md:w-664 md:mt-127 mb-57 text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
         
-        <SubHeading text="Returning Customer" className="text-text39 mb-11 "/>
+        <SubHeading text="Returning Customer" className="text-2xl md:text-text39 mb-11 "/>
            <Flex className="justify-between flex-wrap gap-y-6">
                 <div className='w-48'>
                   <Input text="Email address" placeholder="company@domain.com" type="text"/>
@@ -38,12 +38,16 @@ function Login() {
                 <div className='w-48'>
                   <Input text="Password" placeholder="******" type="text"/>
                 </div>
-              <Button text="Login" className="px-28"/>
+                <Flex className="justify-center md:justify-start">
+                  <Button text="Login" className="md:px-28"/>
+                </Flex>
            </Flex>   
 
-           <SubHeading text="New Customer" className="text-text39 mt-32 "/>
-           <p className='font-dm font-normal text-gray w-664 mt-10  text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
-           <Button text="Continue" className="px-28 mt-10"/>
+           <SubHeading text="New Customer" className="text-2xl md:text-text39 mt-32 "/>
+           <p className='font-dm font-normal text-gray md:w-664 mt-10  text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+           <Flex className="justify-center md:justify-start">
+             <Button text="Continue" className="md:px-28 mt-10"/>
+           </Flex>
         </Container>
     </Section>
   )
